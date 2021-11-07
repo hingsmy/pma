20ae68118b8d6e61ab005258d13cda1f581ef663
hingsmy <hingsmy@gmail.com>
2021-11-07 15:58:50 +0800

    使用@SpringBootTest注解

--- src/test/java/com/hingsmy/pma/ProjectRepositoryIntegrationTest.java
+++ src/test/java/com/hingsmy/pma/ProjectRepositoryIntegrationTest.java
@@ -1,4 +1,4 @@
-package com.hingsmy.dao;
+package com.hingsmy.pma;
 
 import com.hingsmy.pma.PmaApplication;
 import com.hingsmy.pma.dao.ProjectRepository;
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
@@ -14,9 +15,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 
-@ContextConfiguration(classes = PmaApplication.class)
+@SpringBootTest
 @RunWith(SpringRunner.class)
-@DataJpaTest
 @SqlGroup({
         @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = {"classpath:schema.sql", "classpath:data.sql"}),
         @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = {"classpath:drop.sql"})
